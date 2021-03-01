960bf492d324c05b4c9ffe0bc528c536d9e82206
Matteo Boschi <mbmaken@gmail.com>
2021-03-01 16:32:59 +0100

    switch off android instabug

--- android/app/src/main/java/it/pagopa/io/app/MainApplication.java
+++ android/app/src/main/java/it/pagopa/io/app/MainApplication.java
@@ -83,7 +83,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
           new SplashScreenReactPackage(), new ReactNativeExceptionHandlerPackage(), new RNCameraPackage(),
           new ReactNativePushNotificationPackage(), new KeychainPackage(), new RNI18nPackage(), new Sha256Package(),
           new RNMixpanel(), new RNDeviceInfo(), new ReactNativeConfigPackage(),new CiePackage(),new RNPermissionsPackage(),
-          new RNInstabugReactnativePackage.Builder(BuildConfig.INSTABUG_TOKEN, MainApplication.this)
+          new RNInstabugReactnativePackage.Builder("abcde", MainApplication.this)
               .setInvocationEvent("none").setPrimaryColor("#0073E6").build());
     }
 
